80b44786fed530ebd15dab559655d9020fbc6514
Jörn Heinemann <joernheinemann@gmx.de>
2020-02-14 00:23:44 +0100

    Jetzt kann jede Psr 7 Implementation mit der App Factory genutzt werden

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 [![](https://gitlab.com/grammm/php-gram/phpgram/raw/master/docs/img/Feather_writing.svg.png)](https://gitlab.com/grammm/php-gram/phpgram)
 
-# phpgram project
+# phpgram AppFactory
 
 [![Packagist Version](https://img.shields.io/packagist/v/phpgram/framework)](https://packagist.org/packages/phpgram/framework)
 [![PHP from Packagist](https://img.shields.io/packagist/php-v/phpgram/framework)](https://gitlab.com/grammm/php-gram/phpgram-framework/blob/master/composer.json)
 [![Packagist](https://img.shields.io/packagist/l/phpgram/framework)](https://gitlab.com/grammm/php-gram/phpgram-framework/blob/master/LICENSE)
 
-This is the Mvc App Factory for phpgram for the phpgram mvc framework
-
-It create the Request with [Nyholm Psr 7 and Nyholm Psr 7 server](https://github.com/Nyholm/psr7)
+This is the App Factory for phpgram micro framework
--- src/App/AppFactory.php
+++ src/App/AppFactory.php
@@ -11,13 +11,14 @@
  * @author Jörn Heinemann <joernheinemann@gmx.de>
  */
 
-/** @version 1.2.4 */
+/** @version 2.0.0 */
 
 namespace Gram\Project\App;
 
 use Gram\App\App;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7Server\ServerRequestCreator;
+use Gram\ResolverCreator\ResolverCreatorInterface;
+use Gram\Strategy\StrategyInterface;
+use Psr\Container\ContainerInterface;
 
 /**
  * Class AppFactory
@@ -25,35 +26,330 @@ use Nyholm\Psr7Server\ServerRequestCreator;
  *
  * Factory für phpgram App
  *
- * Erstellt Request from globals mit Nyholm Psr
- *
- * Setzt zudem noch Mvc Options wenn die phpgram/framework-lib ebenfalls
- * verwendet werden soll
+ * Hier kann auch eine andere App Class verwendet werden
  */
 class AppFactory
 {
-	use AppFactoryTrait;
+	private static $_instance;
 
+	/** @var App */
+	protected static $gram;
+
+	/** @var Psr7GramBridge */
+	private static $bridge;
+
+	/**
+	 * Startet die App für normale Requests
+	 *
+	 * @return void
+	 */
 	public function start()
 	{
-		//psr 7
-		$psr17Factory=new Psr17Factory();
+		self::app()->setFactory(self::$bridge->getPsr17ResponseFactory());
 
-		$request=new ServerRequestCreator($psr17Factory,$psr17Factory,$psr17Factory,$psr17Factory);
-		$request=$request->fromGlobals();
-
-		self::app()->setFactory($psr17Factory);
-
-		self::app()->start($request);
+		self::app()->start(self::$bridge->createPsr7Request());
 	}
 
-	public function asyncStart():App
+	/**
+	 * Bereitet den Start der App vor für Async Requests
+	 *
+	 * @return App
+	 */
+	public function asyncStart(): App
 	{
-		//psr 7
-		$psr17Factory=new Psr17Factory();
-		self::app()->setFactory($psr17Factory);
+		self::app()->setFactory(self::$bridge->getPsr17ResponseFactory());
 		self::app()->build();
 
 		return self::app();
+	}
+
+	/**
+	 * Setze die App der die Einstellungen zugerechnet werden sollen
+	 *
+	 * @param App $app
+	 */
+	public static function setApp(App $app)
+	{
+		self::$gram = $app;
+	}
+
+	protected static function app(): App
+	{
+		if(self::$gram==null){
+			self::$gram = App::app();
+		}
+
+		return self::$gram;
+	}
+
+	public static function init()
+	{
+		if(!isset(self::$_instance)) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Bestimmte wie Psr 7 Requests erstellt werden
+	 *
+	 * und welche Psr 17 Factory genutzt werden soll
+	 *
+	 * @param Psr7GramBridge $bridge
+	 */
+	public static function setPsrBridge(Psr7GramBridge $bridge)
+	{
+		self::$bridge = $bridge;
+	}
+
+	/**
+	 * Gebe den ResponseCreator der aktuell genutzten App zurück
+	 *
+	 * @return \Psr\Http\Server\RequestHandlerInterface
+	 */
+	public static function getResponseCreator()
+	{
+		return self::app()->getResponseCreator();
+	}
+
+	/**
+	 * Setze Router Options
+	 *
+	 * @param array $routeOptions
+	 */
+	public static function setRouterOptions(array $routeOptions=[])
+	{
+		self::app()->setRouterOptions($routeOptions);
+	}
+
+	/**
+	 * Setze den Resolver Creator der den Resolver aus der gematchten
+	 * Route erstellt
+	 *
+	 * @param ResolverCreatorInterface|null $creator
+	 */
+	public static function setResolverCreator(ResolverCreatorInterface $creator=null)
+	{
+		self::app()->setResolverCreator($creator);
+	}
+
+	/**
+	 * Setze den Psr 11 Container
+	 *
+	 * @param ContainerInterface $container
+	 */
+	public static function setContainer(ContainerInterface $container)
+	{
+		self::app()->setContainer($container);
+	}
+
+	/**
+	 * Setze die Strategy die immer ausgeführt werden soll
+	 *
+	 * @param StrategyInterface $strategy
+	 */
+	public static function setStrategy(StrategyInterface $strategy)
+	{
+		self::app()->setStrategy($strategy);
+	}
+
+	/**
+	 * Bestimmte den Debug Mode
+	 *
+	 * true = Exceptions anzeigen
+	 * false = keine Exceptions zeigen
+	 *
+	 * @param bool $type
+	 */
+	public static function debugMode($type = true)
+	{
+		self::app()->debugMode($type);
+	}
+
+	/**
+	 * @deprecated Legacy Way use @see group() instead
+	 *
+	 * @param $prefix
+	 * @param callable $callback
+	 * @return mixed
+	 */
+	public static function addGroup($prefix, callable $callback)
+	{
+		return self::app()->group($prefix,$callback);
+	}
+
+	/**
+	 * Fügt eine Route group hinzu
+	 *
+	 * @param $prefix
+	 * @param callable $callback
+	 * @return \Gram\Route\RouteGroup
+	 */
+	public static function group($prefix, callable $callback)
+	{
+		return self::app()->group($prefix,$callback);
+	}
+
+	/**
+	 * Fügt eine Route mit beliebiger Method hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @param array $method
+	 * @return \Gram\Route\Route
+	 */
+	public static function add(string $route,$controller,array $method)
+	{
+		return self::app()->add($route,$controller,$method);
+	}
+
+	/**
+	 * Fügt eine GET Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function get(string $route,$controller)
+	{
+		return self::app()->get($route,$controller);
+	}
+
+	/**
+	 * Fügt eine POST Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function post(string $route,$controller)
+	{
+		return self::app()->post($route,$controller);
+	}
+
+	/**
+	 * Fügt eine Route die sowohl GET oder POST sein kann hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function getpost(string $route,$controller)
+	{
+		return self::app()->getpost($route,$controller);
+	}
+
+	/**
+	 * Fügt eine DELETE Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function delete(string $route,$controller)
+	{
+		return self::app()->delete($route,$controller);
+	}
+
+	/**
+	 * Fügt eine PUT Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function put(string $route,$controller)
+	{
+		return self::app()->put($route,$controller);
+	}
+
+	/**
+	 * Fügt eine Patch Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function patch(string $route,$controller)
+	{
+		return self::app()->patch($route,$controller);
+	}
+
+	/**
+	 * Fügt eine HEAD Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function head(string $route,$controller)
+	{
+		return self::app()->head($route,$controller);
+	}
+
+	/**
+	 * Fügt eine Options Route hinzu
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function options(string $route,$controller)
+	{
+		return self::app()->options($route,$controller);
+	}
+
+	/**
+	 * Fügt eine Route hinzu die bei allen Http Methods gematcht wird
+	 *
+	 * @param string $route
+	 * @param $controller
+	 * @return \Gram\Route\Route
+	 */
+	public static function any(string $route,$controller)
+	{
+		return self::app()->any($route,$controller);
+	}
+
+	/**
+	 * Setze den Base Path
+	 *
+	 * @param string $base
+	 */
+	public static function setBase(string $base)
+	{
+		self::app()->setBase($base);
+	}
+
+	/**
+	 * Bestimmte was bei 404 Fehlern ausgeführt werden soll
+	 *
+	 * @param $controller
+	 */
+	public static function notFound($controller)
+	{
+		self::app()->set404($controller);
+	}
+
+	/**
+	 * Bestimmte was bei 405 Fehlern ausgeführt werden soll
+	 *
+	 * @param $controller
+	 */
+	public static function notAllowed($controller)
+	{
+		self::app()->set405($controller);
+	}
+
+	/**
+	 * Setze Middleware die unabhänig der gematchten Route
+	 * ausgeführt wird
+	 *
+	 * @param $middleware
+	 */
+	public static function stdMiddleware($middleware)
+	{
+		self::app()->addMiddleware($middleware);
 	}
 }
